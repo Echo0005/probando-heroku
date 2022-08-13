5becd0df58b27b7d5be049cc8f1a115797c9be3c
Nicolas Echenique <nicolas.echenique.117@gmail.com>
2022-08-13 09:43:12 -0300

    Punto final REST agregado. Heroku error H14 solucionado.

--- src/main/java/com/crud/springboot/MC1M8/controller/Controller.java
+++ src/main/java/com/crud/springboot/MC1M8/controller/Controller.java
@@ -3,9 +3,11 @@ package com.crud.springboot.MC1M8.controller;
 
 import com.crud.springboot.MC1M8.model.Persona;
 import com.crud.springboot.MC1M8.service.IPersonaService;
+import java.time.Instant;
 //import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,15 +15,25 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1.0")
 public class Controller {
     
     @Autowired
     private IPersonaService persoServ;
+    
+    
+    @GetMapping("/time")
+    @ResponseStatus(HttpStatus.OK)
+    public String getCurrentTime(){
+        return Instant.now().toString();
+    }
     
     
     //La siguiente lista sera usado de Base de Datos temporal.
